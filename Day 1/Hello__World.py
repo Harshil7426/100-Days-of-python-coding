d61c7d0ae6c5885052e3733896ffb7bd8d98aec4
Harshil Amin <harshilaminijs@gmail.com>
2024-09-25 21:38:28 +0530

    100 Days of coding

---
+++ Day 1/Hello__World.py
@@ -0,0 +1,10 @@
+print("Harshil Amin")
+print("September 23, 2024")
+
+print("""I am signing up for Replit's 100 days of Python challenge!
+I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day.
+I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happen to be. No excuses for not coding from the middle of a field!""")
+
+print("I am feeling 🥳")
+
+print("You can follow my progress at replit.com/@HarshilAmin1")
