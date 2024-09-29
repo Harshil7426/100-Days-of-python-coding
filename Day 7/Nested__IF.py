3193dd337934f1b3ff0bc0fddff8c0edf3c1b989
Harshil Amin <harshilaminijs@gmail.com>
2024-09-29 19:48:53 +0530

    Day 7

---
+++ Day 7/Nested__IF.py
@@ -0,0 +1,14 @@
+print ("Are you a superfan of 'The Big Bang Theory' or a fake fan? ")
+print()
+print("Answer these questions to find out.")
+
+Glasses = input("Does someone wear glasses? ")
+if Glasses == "yes":
+  print("Correct!")
+else:
+  print("Wrong!")
+  WhoGlasses = input("And who wears glasses? ")
+  if WhoGlasses == "Leonard":
+    print("You got it")
+  else:
+    print("Try again!")
