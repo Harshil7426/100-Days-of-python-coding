6e287b883395d61c152525f22e60b590eee1ae05
Harshil Amin <harshilaminijs@gmail.com>
2024-09-30 16:51:16 +0530

    Day 8

---
+++ Day 8/Affirmation_Generator.py
@@ -0,0 +1,30 @@
+print("Hello. Welcome to your daily affirmation generator.")
+name = input("What is your name? ")
+
+if name =="Mark" or name == "mark":
+ DOW = input("What is the day of the week? ")
+ if DOW == "monday" or DOW == "Monday":
+   print("It is going to be a great Monday", name)
+ if DOW == "tuesday" or DOW == "Tuesday":
+   print("What a wonderful Tuesday it is", name)
+ if DOW == "wednesday" or DOW == "Wednesday":
+   print("Happy Hump Day", name)
+ if DOW == "thursday" or DOW == "Thursday":
+   print(name,"your week is almost over!")
+ if DOW == "friday" or DOW == "Friday":
+   print(name, "It's FRIDAY!")
+
+elif name == "David" or name== "david":
+ DOW = input("What is the day of the week? ")
+ if DOW == "monday" or DOW == "Monday":
+   print("It is going to be a great Monday", name)
+ if DOW == "tuesday" or DOW == "Tuesday":
+   print("You look great in that color", name)
+ if DOW == "wednesday" or DOW == "Wednesday":
+   print("You look chipper today", name)
+ if DOW == "thursday" or DOW == "Thursday":
+   print(name,"you are doing a great job!")
+ if DOW == "friday" or DOW == "Friday":
+   print(name, "it's FRIDAY!")
+else:
+ print("I do not know your name, but I hope you are having a great day!")
---
+++ Day 8/README.md
@@ -0,0 +1,18 @@
+## 👉 Day 8 Challenge
+## Affirmations (or insults) Generator
+
+* Concepts used:
+    * input and output
+    * concatenation
+    * if statements
+    * nested if statements
+Build a custom affirmations generator to give the user a customized affirmation each day of the week.
+
+Make sure you ask the user for their name, the current day of the week, and a few of their favorite things. All of this information should be used to help you build your affirmations.
+The goal is to generate a unique message for each day of the week based on the user's answers.
+Use concatenation to generate the affirmation.
+
+If affirmations are not your jam, then you could do a daily joke or insult. Please don't be mean. Keep it light and funny.
+
+Final challenge: Can you create if statements that do not care about capital or lowercase letters of names.
+
