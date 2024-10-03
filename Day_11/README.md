223cb72a4f3cfee4f09a3d8a5137d612f3165585
Harshil Amin <harshilaminijs@gmail.com>
2024-10-03 17:43:17 +0530

    Day 11

---
+++ Day_11/README.md
@@ -0,0 +1,3 @@
+## 👉 Day 11 Challenge
+# How many seconds are in a year?
+Use the power of breaking a program down into its parts. We could Google this, but why not make a program for this instead.s
---
+++ Day_11/Seconds_in_year.py
@@ -0,0 +1,11 @@
+days_this_year = int(input("How many days are in this year?"))
+
+hours_in_day = 24
+minutes_in_hour = 60
+seconds_in_minute = 60
+
+
+result = days_this_year * hours_in_day * minutes_in_hour * seconds_in_minute
+
+
+print("Number of seconds in a year are", result)
