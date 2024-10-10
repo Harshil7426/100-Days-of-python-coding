14c25b4c675f8507f0a8b61edbca442a7c4dd8e0
Harshil Amin <harshilaminijs@gmail.com>
2024-10-10 14:28:32 +0530

    Day 18

---
+++ Day_18/README.md
@@ -0,0 +1,12 @@
+## 👉 Day 18 Challenge
+# We are going to build a "Guess the Number" guessing game.
+
+- You are going to use a while loop and some of the concepts from the past few days.
+
+- Start by picking a number between 0 and 1,000,000. This will be your first variable.
+- Create a while loop to keep asking the user to guess your number.
+
+- If they are too low, tell them "too low." If they guess too high tell, them "too high."
+- If the user guesses correctly, tell them they are a winner (maybe add a fun emoji too!)
+- Count the number of attempts it took for the user to guess number. Make sure you only show that after they get the answer correct.
+* Extra challenge: If the user types a negative number, exit program.
---
+++ Guess_the_number.py
@@ -0,0 +1,25 @@
+print("Guess the number")
+print("Guess a number between 1 and 1,000,000 and I will tell you if you are too low, too high, or get it correct.")
+print("-------------------------------")
+attempt=0
+correct_number=77777
+while True:
+  number=int(input("What is your guess?: "))
+  if number<0:
+    print("Now we'll never know what the answer is …")
+    exit()
+  elif number<correct_number:
+    print("Too low")
+    attempt+=1
+    continue
+  elif number>correct_number:
+    print("Too high")
+    attempt+=1
+    continue
+  elif number==correct_number:
+    print("Correct! YOU WIN! 🏆")
+    attempt+=1
+    break
+  else:
+    print("Invalid input")
+print(f"It took you {attempt} guesses to get it correct!")
